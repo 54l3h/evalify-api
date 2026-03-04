6e6012ca7d6d61c5de55e8be6616bb881081098b
b477y <mohammedsaleh002@outlook.com>
2026-03-04 20:06:03 +0200

    feat: design RESTful endpoint structure for auth and user resources

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
-@Controller('auth')
+@Controller({
+  path: 'auth',
+  version: '1',
+})
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+  @Post('signup')
+  signUp() {}
+
+  @Post('signin')
+  @HttpCode(HttpStatus.OK)
+  signIn() {}
+
+  @Post('google')
+  @HttpCode(HttpStatus.OK)
+  authenticateWithGoogle() {}
+
+  @Post('verify-otp')
+  @HttpCode(HttpStatus.OK)
+  verifyOtp() {}
+
+  @Post('forgot-password')
+  @HttpCode(HttpStatus.ACCEPTED) // 202: Acknowledged, email process initiated
+  forgotPassword() {}
+
+  @Post('reset-password')
+  @HttpCode(HttpStatus.OK)
+  resetPassword() {}
 }
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 
-@Controller('users')
+@Controller({
+  path: 'users',
+  version: '1',
+})
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+  @Get('me')
+  getMyProfile() {}
+
+  @Get('me/insights')
+  getMyInsights() {}
+
+  @Get('me/courses')
+  getMyEnrolledCourses() {}
+
+  @Get('me/assessments/history')
+  getMyAssessmentsHistory() {}
+
+  @Get('me/assessments/:id/history')
+  getMySpecificAssessmentHistory(@Param('id') assessmentId: string) {}
 }
